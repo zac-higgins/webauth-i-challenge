8801efcee66cd462ae96506d577a1a61d8722bcf
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-16 16:45:06 -0700

    created auth router. Created users db access methods

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,38 @@
+const bcrypt = require('bcryptjs');
 const router = require("express").Router();
+const Users = require('../users/users-model');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.getUsersBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: "Invalid credentials" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
